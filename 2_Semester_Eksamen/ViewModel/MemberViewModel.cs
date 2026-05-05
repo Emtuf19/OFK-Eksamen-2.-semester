63ba2613dfff36168c14e31e077b73ed6cd18e01
Manse <malthe.h.2004@gmail.com>
2026-05-05 09:06:01 +0200

    memberOverView det er en fejl på linje 10, har ikke kunne løse den

--- 2_Semester_Eksamen/ViewModel/MemberViewModel.cs
+++ 2_Semester_Eksamen/ViewModel/MemberViewModel.cs
@@ -4,7 +4,66 @@ using System.Text;
 
 namespace _2_Semester_Eksamen.ViewModel
 {
-    class MemberViewModel
+    
+    public class MemberViewModel : INotifyPropertyChanged
     {
+        public readonly Member _member;
+        
+        private MemberViewModel() : this(new Member()) { }
+    
+        public MemberViewModel(Member member)
+        {
+            _member = member ?? new Member();
+        }
+
+        public int MemberID
+        {
+            get => _member.MemberID;
+            set
+    {
+                if(_member.MemberID !=value)
+                {
+                    _member.MemberID = value;
+                    OnPropertyChanged(nameof(MemberID));
+                }
+            }
+        }
+
+        public string MemberFirstName
+        {
+            get => _member.MemberFirstName ?? string.Empty;
+            set
+            {
+                if ((_member.MemberFirstName ?? string.Empty) != (value ?? string.Empty))
+                {
+                    _member.MemberFirstName = value ?? string.Empty;
+                    OnPropertyChanged(nameof(MemberFirstName));
+                    OnPropertyChanged(nameof(Fullname));
+                }
+            }
+        }
+
+        public string MemberLastName
+        {
+            get => _member.MemberLastName ?? string.Empty:
+            set
+            {
+                if ((_member.MemberLastName ?? string.Empty) != (value ?? string.Empty))
+                {
+                    _member.MemberLastName = value ?? string.Empty;
+                    OnPropertyChanged(nameof(MemberLastName));
+                    OnPropertyChanged(nameof(Fullname));
+                }
     }
+}
+
+        public string Fullname => $"{MemberFirstName} {MemberLastName}".Trim();
+
+        public Member ToModel() => _member;
+
+        public event PropertyChangedEventHandler? Propertychanged;
+        protected void OnPropertyChanged(string propName) => Propertychanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+
+    }
+
 }
--- 2_Semester_Eksamen/Views/OverviewMemberWindow.xaml.cs
+++ 2_Semester_Eksamen/Views/OverviewMemberWindow.xaml.cs
@@ -10,17 +10,87 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using _2_Semester_Eksamen.Model;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+
 
 namespace _2_Semester_Eksamen.Views
 {
-    /// <summary>
-    /// Interaction logic for OverviewMemberWindow.xaml
-    /// </summary>
-    public partial class OverviewMemberWindow : Page
+    /// Interaktion logik for OverviewMemberWindow.xaml
+
+    public partial class OverviewMemberWindow : Page, INotifyPropertyChanged
     {
+
+        private Member? _selecetMember;
+
+        public ObservableCollection<Member> Members { get; } = new ObservableCollection<Member>();
+
+        public Member? SelectedMember
+        {
+            get => _selecetMember;
+            set
+            {
+                if(_selecetMember != value)
+                {
+                    _selecetMember = value;
+                    OnPropertyChanged(nameof(SelectedMember));
+                }
+            }
+        }
+
+
+
         public OverviewMemberWindow()
         {
             InitializeComponent();
+            
+            DataContext = this;
+            LoadMembers();
         }
+
+        private void LoadMembers()
+        {
+            try
+            {
+                var repo = new MemberRepository();
+                var list = repo.GetAll();
+                Members.Clear();
+
+                foreach (var m in list)
+                    Members.Add(m);
+
+                if (Members.Count > 0)
+                    SelectedMember = Members[0];
+        }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show($"kunne ikke vise Medlemer; {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void AddMemberButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditMemberButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteMemberButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SafeMemberButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
     }
 }
