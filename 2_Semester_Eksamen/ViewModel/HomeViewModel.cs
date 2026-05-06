bbf62aee37ae0ae1b87e37c9d495667bcb9040fe
Franz Moeglich <franzmoglich@hotmail.com>
2026-05-06 11:55:22 +0200

    homepage i homeviewmodel

---
+++ 2_Semester_Eksamen/ViewModel/HomeViewModel.cs
@@ -0,0 +1,90 @@
+﻿using _2_Semester_Eksamen.Commands;
+using _2_Semester_Eksamen.Model;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+
+namespace _2_Semester_Eksamen.ViewModel
+{
+    public class HomeViewModel : ViewModelBase
+    {
+        public ObservableCollection<Practice> Practices { get; set; } = new();
+        public ObservableCollection<Event> Events { get; set; } = new();
+
+        public HomeViewModel()
+        {
+            LoadPractices();
+            LoadEvents();
+        }
+
+        //Fejl Meddelelser
+        private string? error;
+        public string? Error
+        {
+            get => error;
+            set
+            {
+                error = value;
+                OnPropertyChanged();
+            }
+        }
+        private string? errorEvent;
+        public string? ErrorEvent
+        {
+            get => errorEvent;
+            set
+            {
+                errorEvent = value;
+                OnPropertyChanged();
+            }
+        }
+        public void LoadPractices()
+        {
+            Practices.Clear();
+
+            var repoPractice = new PracticeRepository();
+            var practicesFromDb = repoPractice.GetAll();
+
+            // hvis db er tom
+            if (practicesFromDb == null || practicesFromDb.Count == 0)
+            {
+                Error = "Ingen Træning";
+            }
+            // hvis db ikke er tom
+            else
+            {
+                foreach (var practice in practicesFromDb)
+                {
+                    Practices.Add(practice);
+                }
+            }
+        }
+        public void LoadEvents()
+        {
+            Events.Clear();
+            //Events 
+
+            var repoEvent = new EventRepository();
+            var eventsFromDb = repoEvent.GetAll();
+
+            // hvis db er tom
+            if (eventsFromDb == null || eventsFromDb.Count == 0)
+            {
+               ErrorEvent = "Ingen Events";
+            }
+            // hvis db ikke er tom
+            else
+            {
+                foreach (var ev in eventsFromDb)
+                {
+                    Events.Add(ev);
+                }
+            }
+        }
+    }
+}
--- 2_Semester_Eksamen/Views/HomeWindow.xaml.cs
+++ 2_Semester_Eksamen/Views/HomeWindow.xaml.cs
@@ -5,63 +5,16 @@ using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
+using _2_Semester_Eksamen.ViewModel;
 
 namespace _2_Semester_Eksamen.Views
 {
     public partial class HomeWindow : Page
     {
-        public ObservableCollection<Practice> Practices { get; set; }
-        public ObservableCollection<Event> Events { get; set; }
-      
-        //Fejl Meddelelser
-        public string? Error { get; set; }
-        public string? ErrorEvent { get; set; }
-
         public HomeWindow()
         {
             InitializeComponent();
-            // Pratice 
-            Practices = new ObservableCollection<Practice>();
-
-            var repoPractice = new PracticeRepository();
-            var practicesFromDb = repoPractice.GetAll();
-
-            // hvis db er tom
-            if (practicesFromDb == null)
-            {
-                Error = "Ingen Trænning";
-                TrainingError_txt.Visibility = Visibility.Visible;
-            }
-            // hvis db ikke er tom
-            else
-            {
-                foreach (var practice in practicesFromDb)
-                {
-                    Practices.Add(practice);
-                }
-            }
-
-            //Events 
-            Events = new ObservableCollection<Event>();
-
-            var repoEvent = new EventRepository();
-            var eventsFromDb = repoEvent.GetAll();
-
-            // hvis db er tom
-            if (eventsFromDb == null)
-            {
-                ErrorEvent = "Ingen Events";
-                EventError_txt.Visibility = Visibility.Visible;
-            }
-            // hvis db ikke er tom
-            else
-            {
-                foreach (var ev in eventsFromDb)
-                {
-                    Events.Add(ev);
-                }
-            }
-            DataContext = this;
+            DataContext = new HomeViewModel();
         }
     }
 }
